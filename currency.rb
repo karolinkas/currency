c5d1a4153d14014e89045e87eceaa845e596e40c
Hayri <hello@hoshilab.com>
2014-09-23 10:58:51 +0200

    RED test

--- currency.rb
+++ currency.rb
@@ -1,6 +1,6 @@
 class Calculator
 
-	def calculate_to_euro
+	def calculate_to_euro(arg1)
 		0.77818
 	end
 
@@ -11,16 +11,19 @@ end
 
 
 describe Calculator do 
-	before @super_calculator = Calculator.new
+	before do 
+    @super_calculator = Calculator.new
+  end
+
 	
 	describe :calculate_to_euro do
 		it "should return the value in dollars" do
 			@super_calculator.calculate_to_euro(1).should==0.77818
 		end
 
-		
-
-
+    it "should convert swedish kr to euro" do
+      @super_calculator.calculate_to_euro(100).should == 11.29
+    end
 	end
 end
 
